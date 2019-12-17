056521a235b3619a16f5cdcdce0f340c0328468f
Cao Kai <caokai2@lenovo.com>
2019-12-17 17:50:35 +0800

    first commit

---
+++ kubernetes/kubectl-apply.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "JHipster Console - http://jhipster-console.default."
+    echo "#####################################################"
+}
+
+kubectl apply -f registry/
+kubectl apply -f gateway/
+kubectl apply -f blog/
+kubectl apply -f store/
+kubectl apply -f console/
+
+logSummary
